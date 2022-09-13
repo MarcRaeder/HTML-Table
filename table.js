22c3eb8affd7959489b13423c345e909245ddd47
Marc Räder <marcrader@Marcels-MBP.office.5minds.de>
2022-09-13 15:56:46 +0200

    add change_columnsize & fix codestyle

--- table.js
+++ table.js
@@ -1,31 +1,90 @@
-function ShowHide(title) {
+const border = document.querySelectorAll(".border");
+const columns = document.querySelectorAll(".table-headerRow__body")
+const table = document.querySelector(".table");
 
-  if (title == 1) {
-    let title_data = document.querySelectorAll(".table-contentRow__name");
-    CheckIfShowOrHide(title_data);
-  }
-
-  else if (title == 2) {
-    let title_data = document.querySelectorAll(".table-contentRow__age");
-    CheckIfShowOrHide(title_data);
-  }
-  else if (title == 3) {
-    let title_data = document.querySelectorAll(".table-contentRow__country");
-    CheckIfShowOrHide(title_data);
-  }
-  else if (title == 4) {
-    let title_data = document.querySelectorAll(".table-contentRow__color");
-    CheckIfShowOrHide(title_data);
-  }
+function ShowHide(columnId) {
+  let columnData = document.querySelectorAll("#" + columnId);
+  CheckIfShowOrHide(columnData);
 }
 
-function CheckIfShowOrHide(title_data) {
-  title_data.forEach((data) => {
+function CheckIfShowOrHide(columnData) {
+  columnData.forEach((data) => {
     if (data.style.color != "transparent") {
       data.style.color = "transparent";
     }
     else if (data.style.color == "transparent") {
       data.style.color = "black";
     }
+  })
+}
+
+for (let i = 0; i < border.length; i++) {
+  if (i == 0) {
+    border[i].style.left = `${table.offsetLeft + columns[i].offsetWidth}px`;
+  }
+  else {
+    border[i].style.left = `${border[i - 1].offsetLeft + columns[i].offsetWidth}px`;
+  }
+}
+
+function resize() {
+  for (let i = 0; i < border.length; i++) {
+    if (i == 0) {
+      border[i].style.left = `${table.offsetLeft + columns[i].offsetWidth}px`;
+    }
+    else {
+      border[i].style.left = `${border[i - 1].offsetLeft + columns[i].offsetWidth}px`;
+    }
+  }
+}
+
+document.getElementsByTagName("BODY")[0].onresize = () => resize();
+
+function dragstart(event, borderNumber) {
+  const minWidth = 100;
+  const mousestart = event.clientX;
+  var borderNum = borderNumber;
+  var leftColumn = columns[borderNum];
+  var leftColumnWidth = columns[borderNum].offsetWidth;
+  var previousColumsWidth = 0;
+  var rightColumn = columns[borderNum + 1];
+  var rightColumnWidth = columns[borderNum + 1].offsetWidth;
+
+  if (borderNumber > 0) {
+    for (let i = 0; i < borderNumber; i++) {
+      previousColumsWidth += columns[i].offsetWidth;
+    }
+  }
+
+  function mousemove(event) {
+    var mouseend = event.clientX;
+    var mouseDifference = mousestart - mouseend;
+    var leftborder = leftColumnWidth < minWidth && event.clientX >= (previousColumsWidth + minWidth);
+    var outOfBorder = event.clientX < (previousColumsWidth + minWidth) || event.clientX > (previousColumsWidth + leftColumnWidth + (rightColumnWidth - minWidth));
+    var rightborder = rightColumnWidth < minWidth && event.clientX <= (previousColumsWidth + leftColumnWidth + minWidth);
+
+    if (leftborder) {
+      border[borderNum].style.left = `${mouseend}px`;
+      leftColumn.style.width = `${leftColumnWidth + mouseDifference}px`;
+      rightColumn.style.width = `${rightColumnWidth - mouseDifference}px`;
+    }
+    else if (rightborder) {
+      border[borderNum].style.left = `${mouseend}px`;
+      leftColumn.style.width = `${leftColumnWidth - mouseDifference}px`;
+      rightColumn.style.width = `${rightColumnWidth + mouseDifference}px`;
+    }
+    else if (outOfBorder) {
+      return false;
+    }
+    else {
+      border[borderNum].style.left = `${mouseend}px`;
+      leftColumn.style.width = `${leftColumnWidth - mouseDifference}px`;
+      rightColumn.style.width = `${rightColumnWidth + mouseDifference}px`;
+    }
+  }
+
+  document.addEventListener("mousemove", mousemove);
+  document.addEventListener("mouseup", (event) => {
+    document.removeEventListener("mousemove", mousemove)
   })
 }
