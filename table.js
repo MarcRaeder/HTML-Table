f4defabb12f7ccd0ac6ec7f61f0c00cfb6058535
Marc Räder <marcrader@Marcels-MBP.office.5minds.de>
2022-09-15 08:41:16 +0200

    fix codestyle

--- table.js
+++ table.js
@@ -1,26 +1,10 @@
-function ShowHide(title) {
-
-  if (title == 1) {
-    let title_data = document.querySelectorAll(".table-contentRow__name");
-    CheckIfShowOrHide(title_data);
-  }
-
-  else if (title == 2) {
-    let title_data = document.querySelectorAll(".table-contentRow__age");
-    CheckIfShowOrHide(title_data);
-  }
-  else if (title == 3) {
-    let title_data = document.querySelectorAll(".table-contentRow__country");
-    CheckIfShowOrHide(title_data);
-  }
-  else if (title == 4) {
-    let title_data = document.querySelectorAll(".table-contentRow__color");
-    CheckIfShowOrHide(title_data);
-  }
+function ShowHide(columnClass) {
+  let columnData = document.querySelectorAll("." + columnClass);
+  CheckIfShowOrHide(columnData);
 }
 
-function CheckIfShowOrHide(title_data) {
-  title_data.forEach((data) => {
+function CheckIfShowOrHide(columnData) {
+  columnData.forEach((data) => {
     if (data.style.color != "transparent") {
       data.style.color = "transparent";
     }
